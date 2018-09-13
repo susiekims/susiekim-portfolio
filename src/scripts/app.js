533374de93d89332c537ea8c86e8f00ac49685b1
Susie <susie.kim@hotmail.com>
2018-09-13 13:19:34 -0400

    added dark theme

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,30 @@
-const hello = 'World';
-console.log(hello);
+
+const lights = () => {
+    $('#lights').on('click', () => {
+        console.log('click');
+        $('body').toggleClass('dark');
+        $('.intro').toggleClass('dark-border-bottom');
+        $('.work-wrapper').toggleClass('dark-background');
+        $('.work-wrapper h2').toggleClass('dark');
+        $('.dev').toggleClass('dark-border-right');
+        $('.work').toggleClass('dark-border-bottom');
+        if ($('body').hasClass('dark')) {
+            $('#light-emoji').text('🌚');
+        } else {
+            $('#light-emoji').text('🌞');
+        }
+    })
+}
+
+const changeEmoji = () => {
+    $( document ).on( "mousemove",() => {
+        $('#emoji').text(data.emojis[Math.floor(Math.random() * data.emojis.length)]);
+    });
+}
+
+
+$(function() {
+    console.log('hey');
+    changeEmoji();
+    lights();
+});
