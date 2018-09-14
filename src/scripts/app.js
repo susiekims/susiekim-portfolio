6465fa5ca7afba0aceac07c4bd099e1787d55e72
Susie <susie.kim@hotmail.com>
2018-09-14 01:18:58 -0400

    changed layout

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,30 +1,78 @@
 
-const lights = () => {
-    $('#lights').on('click', () => {
-        console.log('click');
-        $('body').toggleClass('dark');
-        $('.intro').toggleClass('dark-border-bottom');
-        $('.work-wrapper').toggleClass('dark-background');
-        $('.work-wrapper h2').toggleClass('dark');
-        $('.dev').toggleClass('dark-border-right');
-        $('.work').toggleClass('dark-border-bottom');
-        if ($('body').hasClass('dark')) {
-            $('#light-emoji').text('🌚');
-        } else {
-            $('#light-emoji').text('🌞');
-        }
-    })
-}
 
 const changeEmoji = () => {
-    $( document ).on( "mousemove",() => {
+    $( document ).on( "mousemove",(e) => {
         $('#emoji').text(data.emojis[Math.floor(Math.random() * data.emojis.length)]);
     });
 }
+
+const showSection = (element) => {
+    if ( $(window).width() > 600) {
+        $(`.${element}`).on('click', function() {
+            $(`section`).css({width: '10vw'});
+            $(`.${element}`).css({width: '80vw'});
+        })
+    } else {
+        $(`.${element}`).on('click', function() {
+            $(`section`).css({width: '100vw'});
+            $(`section`).css({height: '10vh'});
+            $(`.${element}`).css({height: '80vh'});
+        })
+    }
+}
+
+
+// const changeLayout = (section, notThisSection, notThisEither) => {
+//     let element = $(`.${section}`);
+//     let element2 = $(`.${notThisSection}`);
+//     let element3 = $(`.${notThisEither}`);
+//     element.mouseover(function(e) {
+//         console.log(e.target);
+//         element.animate({width: '35%'}, 100);
+//         element2.animate({width: '32.5%'}, 100);
+//         element3.animate({width: '32.5%'}, 100);
+//     });
+//     element.mouseleave(function() {
+//         element.animate({width: `${100/3}%`}, 100);
+//         element2.animate({width: `${100/3}%`}, 100);
+//         element3.animate({width: `${100/3}%`}, 100);
+//     });
+// }
+
+// $(window).on('resize', () => {
+//     if ( $(window).width() > 600 ) {
+//         changeLayout('designer', 'developer', 'susie');
+//         changeLayout('developer', 'designer', 'susie');
+//     } else {
+//         $('section').css('width', '100%');
+//     }
+// })
 
 
 $(function() {
     console.log('hey');
     changeEmoji();
-    lights();
+    showSection('developer')
+    showSection('susie')
+    showSection('designer')
+    $(window).on('resize', function() {
+        let windowWidth = $(window).width();
+        if (windowWidth < 600) {
+            $('section').css({
+                width: '100%',
+                height: '33.333vh'
+            });
+            showSection('developer')
+            showSection('susie')
+            showSection('designer')
+        } else {
+            $('section').css({
+                width: '33.333%',
+                height: '100vh'
+            });
+            showSection('developer')
+            showSection('susie')
+            showSection('designer')
+        }
+    })
 });
